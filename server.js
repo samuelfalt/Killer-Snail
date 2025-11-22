32359e457bf028dc5330d13b614ce19bf4deb317
Samuel Fält <samuel@Samuel-MacBook-Air-2.local>
2025-11-22 22:21:39 +0200

    Add HTTP response for Render health check

--- server.js
+++ server.js
@@ -240,7 +240,10 @@ function cleanup(ws) {
   }
 }
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("ok");
+});
 const wss = new WebSocketServer({ server });
 
 let idSeq = 1;
